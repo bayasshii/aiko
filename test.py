dcb6d97bfd3d9b2cb36ea946505f2ac11dd242af
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-05-07 11:47:49 +0900

    python追加

---
+++ test.py
@@ -0,0 +1,11 @@
+import glob
+files = glob.glob('./songs/*')
+print(len(files))
+
+for file in files:
+    f = open(file+'/lyrics.txt')
+    #print(file)
+    Allf = f.read()
+    text = Allf.replace('\n','')
+    text = text.replace('\r','')
+     #print(text, end ="")
